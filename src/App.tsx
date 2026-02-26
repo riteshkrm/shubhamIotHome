6d1e47b33600939b72d308e27cc2c4c6c0527a68
riteshkrm <rtskrm@gmail.com>
2026-02-26 13:09:04 +0530

    create folder structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>hdfdfhdfh</Text>
+    </View>
+  );
+}
+
+export default App;
